c39a00a3861d8b54b74530a5ba7f144fadd8a1c8
Daniel Tull <dt@danieltull.co.uk>
2012-09-25 17:05:29 +0100

    Initial Commit

---
+++ DCTRemoteSettings/DCTRemoteSettings.h
@@ -0,0 +1,13 @@
+//
+//  DCTRemoteSettings.h
+//  DCTRemoteSettings
+//
+//  Created by Daniel Tull on 25.09.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTRemoteSettings : NSObject
+
+@end
---
+++ DCTRemoteSettings/DCTRemoteSettings.m
@@ -0,0 +1,13 @@
+//
+//  DCTRemoteSettings.m
+//  DCTRemoteSettings
+//
+//  Created by Daniel Tull on 25.09.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTRemoteSettings.h"
+
+@implementation DCTRemoteSettings
+
+@end
